958abdc0d96b3e6cd7900e17b4f51b5bfca750b6
2022KirillMaximov <102306817+2022KirillMaximov@users.noreply.github.com>
2024-04-15 12:11:12 +0200

    new update

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,37 +1,37 @@
 import './App.css'
-import { useState,useEffect  } from "react"
+import { useState, useEffect } from "react"
 import ContactList from '../ContactList/ContactList'
 import SearchBox from '../SearchBox/SearchBox'
 import ContactForm from '../ContactForm/ContactForm'
 
-export default function App() {
-
-
-
- const [contacts, setContacts] = useState(() => {
-    const savedContacts = window.localStorage.getItem("saved-contact");
-    if (savedContacts !== null) {
-      return JSON.parse(savedContacts);
-    }
-    return [
+const initialContacts = [
   {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
   {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
   {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
   {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
-]
-;
+];
+
+export default function App() {
+  const [contacts, setContacts] = useState(() => {
+    const savedContacts = window.localStorage.getItem("saved-contact");
+    if (savedContacts !== null) {
+      return JSON.parse(savedContacts);
+    }
+    return initialContacts; // Вот здесь мы используем массив как начальное значение
   });
 
   useEffect(() => {
     window.localStorage.setItem("saved-contact", JSON.stringify(contacts));
   }, [contacts]);
- const [filter, setFilter] = useState('');
-const handleFilterChange = event => {
-  setFilter(event.target.value);
-};
- const filteredContacts = contacts.filter(contact =>
+
+  const [filter, setFilter] = useState('');
+  const handleFilterChange = event => {
+    setFilter(event.target.value);
+  };
+
+  const filteredContacts = contacts.filter(contact =>
     contact.name.toLowerCase().includes(filter.toLowerCase())
-   );
+  );
   
   const addContact = (newContact) => {
     setContacts((prevContacts) => {
@@ -39,27 +39,18 @@ const handleFilterChange = event => {
     });
   };
 
-const deleteContact = (contactId) => {
-  const updatedContacts = contacts.filter((contact) => contact.id !== contactId);
-  localStorage.setItem("contacts", JSON.stringify(updatedContacts));
-  setContacts(updatedContacts);
-};
-
-  
+  const deleteContact = (contactId) => {
+    const updatedContacts = contacts.filter((contact) => contact.id !== contactId);
+    localStorage.setItem("saved-contact", JSON.stringify(updatedContacts));
+    setContacts(updatedContacts);
+  };
 
   return (
-
-
     <>
       <h1>Phonebook</h1>
       <ContactForm onAdd={addContact} />
       <SearchBox value={filter} onChange={handleFilterChange} />
-   
-  
-      <ContactList contacts={filteredContacts } onDelete={deleteContact}/>
-      
+      <ContactList contacts={filteredContacts} onDelete={deleteContact} />
     </>
-  )
+  );
 }
-
-
